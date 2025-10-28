367d57ea7a4076a7911f9402fd3bda4e40c0fcda
Caison Blake Lewis <caison.lewis@gmail.com>
2025-10-28 17:11:33 -0400

    change to front page image

--- websiteB/wwwroot/js/index.js
+++ websiteB/wwwroot/js/index.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Array of image sources
     const imageSources = [
         "/BlizzNet/websiteB/wwwroot/images/sadboi.png",
-        "/BlizzNet/websiteB/wwwroot/images/sad-cat-rolling.gif",
+        "/BlizzNet/websiteB/wwwroot/images/ja.gif",
         "/BlizzNet/websiteB/wwwroot/images/blastoise-water.gif"
     ];
 
